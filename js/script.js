9192dd9e7361a41cd665d7c8232df76ce22ff5fd
Stuard Quiroz <stuuard96@gmail.com>
2022-04-13 18:49:49 -0500

    Se arreglo el nav y se completo la seccion learning

---
+++ js/script.js
@@ -0,0 +1,32 @@
+/*variables de clases*/
+const bars_menu = document.querySelector(".nav__bar");
+const menu = document.querySelector(".nav__menu");
+const lista = document.querySelector(".nav__menu ul");
+const cursor = document.querySelectorAll(".nav__menu--a");
+const span__bars = document.querySelectorAll(".nav__bar span");
+
+/*Ejecución de las funciones*/
+bars_menu.addEventListener("click", animateBars);
+bars_menu.addEventListener("click", toggleMenu);
+bars_menu.addEventListener("click", cursorStyle);
+
+/*-------------------------Menu Hamburguesa-----------------*/
+function animateBars(){
+    span__bars.forEach( (i, index)=>{
+        index+1;
+        i.classList.toggle("nav__bar--active"+index)
+    });
+}
+
+/*--------------------Close Menu--------------------------*/
+function toggleMenu(){
+    menu.classList.toggle("toggle__menu");  
+    lista.classList.toggle("toggle__menu");
+}
+
+/*--------------Quitar estilo del Cursor-------------------*/
+function cursorStyle(){
+    cursor.forEach(i=>i.style.setProperty('cursor', 'pointer'));
+}
+
+
