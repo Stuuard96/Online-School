5e54406bdaaee0a19dbe9172dad5927835d906be
Stuard Quiroz <stuuard96@gmail.com>
2022-04-16 22:42:05 -0500

    Cambios en el header mobile y desktop

--- js/script.js
+++ js/script.js
@@ -1,29 +1,50 @@
-/*variables de clases*/
+/*-------------------variables de clases--------------------*/
 const bars_menu = document.querySelector(".nav__bar");
 const menu = document.querySelector(".nav__menu");
 const cursor = document.querySelectorAll(".nav__menu--a");
 const span__bars = document.querySelectorAll(".nav__bar span");
 
 
-/*Ejecución de las funciones*/
-bars_menu.addEventListener("click", animateBars);
-bars_menu.addEventListener("click", toggleMenu);
-bars_menu.addEventListener("click", cursorStyle);
+/*------------Ejecución de las funciones---------------------*/
+/* bars_menu.addEventListener("click", animateBars);
+bars_menu.addEventListener("click", toggleMenu); */
+/* bars_menu.addEventListener("click", cursorStyle); */
+/* cursor.addEventListener("click", closeMenu);*/
 
-/*-------------------------Close Hamburguesa-----------------*/
-function animateBars(){
-    span__bars.forEach( (i, index)=>{
-        i.classList.toggle("nav__bar--active"+index)
+/* $('.nav__menu--a').click(() => {
+    $('.nav__menu').removeClass('toggle__menu');
+    
+    $('.nav__bar > span').forEach((i,index)=>{
+        i.classList.removeClass("nav__bar--active"+index);
+    });       
+}); */
+
+/* $(document).ready(function(){
+    $(".nav__menu--a").click(function(){
+        $('.nav__menu').removeClass('toggle__menu');
+        for(var i=0;i<$('.nav__bar > span').length;i++){
+            i.classList.removeClass("nav__bar--active"+index);
+           }
     });
-}
+}); */
+
+
+/* function closeMenu(){
+    menu.classList.remove("toggle__menu");
+    span__bars.forEach( (i, index)=>{
+        i.classList.remove("nav__bar--active"+index)
+    }); 
+} */
+
 
 /*--------------------Close Menu--------------------------*/
-function toggleMenu(event){
-    menu.classList.toggle("toggle__menu");  
-}
+/* function toggleMenu(){
+    menu.classList.toggle("toggle__menu");
+} */
 
 /*--------------Quitar estilo del Cursor-------------------*/
-function cursorStyle(){
+/* function cursorStyle(){
     cursor.forEach(i=>i.style.setProperty('cursor', 'pointer'));
-}
+} */
  
+
