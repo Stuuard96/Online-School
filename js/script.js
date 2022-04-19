c98f2bc4cb4a85140def8622aab2d44c56d88f96
Stuard Quiroz <stuuard96@gmail.com>
2022-04-19 16:28:23 -0500

    Termine version mobile y desktop

--- js/script.js
+++ js/script.js
@@ -1,27 +1,22 @@
 /*-------------------variables de clases--------------------*/
 const bars__open = document.querySelector("#nav__open");
-const bars__close = document.querySelector("#nav__close");
 const menu = document.querySelector(".nav__menu--content");
-/* const cursor = document.querySelectorAll(".nav__menu--a");
-const span__bars = document.querySelectorAll(".nav__bar span"); */
-
 
 /*------------Ejecución de las funciones---------------------*/
 bars__open.addEventListener("click", toggleMenu);
-bars__close.addEventListener("click", closeMenu);
 
 /*--------------------Close Menu--------------------------*/
 function toggleMenu(){
     menu.classList.toggle("toggle__menu");
-    bars__open.style.display='none';
-    bars__close.style.display='block';
+
+    if(bars__open.classList.contains('fa-bars')){
+        bars__open.classList.replace('fa-bars','fa-xmark');
+    }else{
+        bars__open.classList.replace('fa-xmark','fa-bars');
+    }
 }
 
-function closeMenu(){
-    menu.classList.remove("toggle__menu");
-    bars__open.style.display='block'
-    bars__close.style.display='none';
-}
+
 
 
 
